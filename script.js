93dcf0989e2346b9bbe1e5d4f458c66e7df6cb20
Roselyn Marte <rjcutie15@gmail.com>
2026-06-30 22:13:37 -0400

    phase 1 complete

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const gameTitle = document.getElementById("game-title")
-const scoreDisplay = document.getElementById("score")
+// getElementById finds one element whose attribute matches the string exactly
+const gameTitle = document.getElementById("game-title");
+const scoreDisplay = document.getElementById("score");
 // select #question-number  → store in questionNumber
-const questionNumber = document.getElementById("question-number")
+const questionNumber = document.getElementById("question-number");
 
 // select #question-text    → store in questionText
-const questionText = document.getElementById("question-text")
+const questionText = document.getElementById("question-text");
 // select #question-card    → store in questionCard
-const questionCard = document.getElementById("question-card")
+const questionCard = document.getElementById("question-card");
 // select #answer-list      → store in answerList
-const answerList = document.getElementById("answer-list")
+const answerList = document.getElementById("answer-list");
 // select #next-btn         → store in nextBtn
-const nextBtn = document.getElementById("next-btn")
+const nextBtn = document.getElementById("next-btn");
 // select #end-screen       → store in endScreen
-const endScreen = document.getElementById("end-screen")
+const endScreen = document.getElementById("end-screen");
 
-const answerBtnsCollection = document.getElementsByClassName("answer-btn")
+//getElementsByClassName finds all the elements with that class name and returns an html collection
+const answerBtnsCollection = document.getElementsByClassName("answer-btn");
 // select ".answer-btn" using querySelectorAll → store in answerBtnsNodeList
-const answerBtnsCollection = document.querySelectorAll(".answer-btn")
+
+//querySelectorAll finds all elements mathching a css selector and returns a node list
+const answerBtnsNodeList = document.querySelectorAll(".answer-btn");
+
+console.log(answerBtnsCollection);
+console.log(answerBtnsNodeList);
+
+const btnsArray = Array.from(answerBtnsNodeList)
+//getElementsByClassName returns an htmlcollection.
+// querySelectorAll returns a node list.
+// To use .map() on either, convert with convert with Array.from() or spread syntax [...].
+
 
